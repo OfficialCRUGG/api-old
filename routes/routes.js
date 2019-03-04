17d1f71a4a5e82b0423bf1e07350e3908abd3ba7
OfficialCRUGG <mrcreeper2011@web.de>
2019-03-04 18:58:51 +0100

    Added /amount entpoints

--- routes/routes.js
+++ routes/routes.js
@@ -7,7 +7,7 @@ var appRouter = function (app) {
   });
 
   app.get("/randomgame", function(req, res) {
-    res.status(200).send(`Welcome to the randomgame api. We currently have ${games.length} games in our database!\n\nEndpoints:\n/all - Display all games from our database\n/random - Display a random game from our database`);
+    res.status(200).send(`Welcome to the randomgame api. We currently have ${games.length} games in our database!\n\nEndpoints:\n/all - Display all games from our database\n/random - Display a random game from our database\n/amount - Gives you the amount of entries in our database`);
   });
 
   app.get("/randomgame/all", function(req, res) {
@@ -19,8 +19,12 @@ var appRouter = function (app) {
     res.status(200).send(data);
   });
 
+  app.get("/randomgame/amount", function(req, res) {
+    res.status(200).send(games.length);
+  });
+
   app.get("/cute", function(req, res) {
-    res.status(200).send(`Welcome to the cute api. We currently have ${cute.length} images and gifs in our database!\n\nEndpoints:\n/all - Display all images and gifs from our database\n/random - Display a random image or gif from our database`);
+    res.status(200).send(`Welcome to the cute api. We currently have ${cute.length} images and gifs in our database!\n\nEndpoints:\n/all - Display all images and gifs from our database\n/random - Display a random image or gif from our database\n/amount - Gives you the amount of entries in our database`);
   });
 
   app.get("/cute/all", function(req, res) {
@@ -30,6 +34,10 @@ var appRouter = function (app) {
   app.get("/cute/random", function(req, res) {
     let data = cute[Math.floor(Math.random()*cute.length)];
     res.status(200).send(data);
+  });
+
+  app.get("/cute/amount", function(req, res) {
+    res.status(200).send(cute.length);
   });
 }
 
